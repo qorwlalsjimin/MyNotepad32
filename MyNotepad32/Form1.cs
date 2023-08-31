3b712e9990adbdd6a48a24ee29f03afa5fc99f76
Jimin Baek <qorwlalsjimin@naver.com>
2023-08-31 10:03:25 +0900

    파일 열기 구현

--- MyNotepad32/Form1.cs
+++ MyNotepad32/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,12 @@ namespace MyNotepad32
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void 열기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog(this);
+            textBox1.Text = File.ReadAllText(openFileDialog1.FileName);
         }
     }
 }
